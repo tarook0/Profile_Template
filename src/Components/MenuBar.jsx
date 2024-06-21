f00b5e5f9f0226399a03488b09de0647bb2c3bd9
tarook331@gmail.com <tarook331@gmail.com>
2024-06-21 16:41:49 +0300

    finish page header .

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,12 +1,32 @@
+import styled from "styled-components";
 import { Inline } from "../Ui/Inline";
+import { Pad } from "../Ui/Pad";
+
+const Menu = styled(Inline).attrs(() => ({
+  as: Pad,
+  gutter: "l",
+  padding: ["m", "xl"],
+  align: "center",
+  stretch: 1,
+}))`
+  background-color: #210551;
+  color: wheat;
+  border-block-end: 1px solid #0c021e;
+`;
 
 export default function MenuBar() {
   return (
     <div>
-      <Inline gutter="l" align="center" stretch={1}>
+      <Menu
+        as={Pad}
+        padding={["m", "xl"]}
+        gutter="l"
+        align="center"
+        stretch={1}
+      >
         <div>logo</div>
         <nav>
-          <Inline gutter='s' as={'ul'}>
+          <Inline gutter="s" as={"ul"}>
             <li>Overview</li>
             <li>Position</li>
             <li>Candidates</li>
@@ -16,7 +36,7 @@ export default function MenuBar() {
         <input placeholder="search" />
         <div>icon</div>
         <div>Profile</div>
-      </Inline>
+      </Menu>
     </div>
   );
 }
